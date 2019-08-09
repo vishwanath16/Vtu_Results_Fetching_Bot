cb3c9eddf54ccfef184f98eb6893c28461a13956
VISHWANATH T S <31544005+vishwanath16@users.noreply.github.com>
2019-08-09 21:31:20 +0530

    Add files via upload

---
+++ VTU_Result_Bot.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class vtu:
+    def login(self, usn):
+        self.usn = usn
+        self.bot = webdriver.Firefox()
+        bot = self.bot
+        bot.get('https://www.vtu4u.com')
+        time.sleep(4)
+        btn = bot.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/form/div/div/div[1]/div[2]')  #XPath of CBCS Tab
+        btn.click()
+        text = bot.find_element_by_xpath('//*[@id="usn"]')  #XPath of usn field
+        text.send_keys(usn)
+        text.submit()
+
+
+vish = vtu()
+vish.login('1ap18cs040')
